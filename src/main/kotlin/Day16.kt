42254b046c214cd7b1c1da191f9206fc91e438f4
adamfranks <adam@adamfranks.com>
2022-12-17 03:29:42 -0500

    Day 17/16

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -6,22 +6,23 @@ import java.io.File
 import java.lang.Integer.max
 import java.lang.Integer.parseInt
 import java.util.*
+import kotlin.collections.ArrayDeque
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 
 fun main() {
-    val lines = File("Day16test.csv").useLines { it.toList() }
+    val lines = File("Day16.csv").useLines { it.toList() }
 
     val tunnels = HashMap<String, Tunnel>()
 
-    for(l in lines) {
+    for (l in lines) {
         val split = l.split(",")
         val name = split.first()
         val flowRate = split[1]
 
-        val tunnel = Tunnel(name, hashSetOf<String>(), parseInt(flowRate), hashMapOf())
-        for(i in 2 until split.size) {
+        val tunnel = Tunnel(name, hashSetOf<String>(), parseInt(flowRate), TreeMap())
+        for (i in 2 until split.size) {
             tunnel.neighbors.add(split[i])
         }
 
@@ -29,10 +30,10 @@ fun main() {
     }
 
     println(tunnels)
-    for(t in tunnels) {
+    for (t in tunnels) {
         calcDist(tunnels, t.value, t.value, hashSetOf(), 0)
-        val newDist = t.value.dist.filter { tunnels[it.key]!!.flowRate != 0 }
-        t.value.dist = newDist as HashMap<String, Int>
+//        val newDist = t.value.dist.filter { tunnels[it.key]!!.flowRate != 0 && it.key != t.key}
+//        t.value.dist = newDist as TreeMap<String, Int>
     }
 
 //
@@ -42,101 +43,186 @@ fun main() {
 //        totalReleased += perMinute
 //
 //    }
-    val memo = ArrayList<HashMap<HashSet<String>, HashMap<HashSet<String>, Int>>>()
-    val memoOff = ArrayList<HashMap<String, Int>>()
+    val memo = ArrayList<ArrayList<HashMap<HashSet<String>, HashMap<Pair<String, String>, Int>>>>()
 
-    for(i in 0..30) {
-        memo.add(hashMapOf())
-        memoOff.add(hashMapOf())
+    for (i in 0..30) {
+        val new = ArrayList<HashMap<HashSet<String>, HashMap<Pair<String, String>, Int>>>()
+        memo.add(new)
+        for (j in 0..30) {
+            new.add(hashMapOf())
+        }
     }
 
-    println(calcMax(1, tunnels["AA"]!!, tunnels, memo, hashSetOf(""), tunnels["AA"]!!))
+    println(calcMax(1, 1, tunnels["AA"]!!, tunnels, memo, hashSetOf(""), tunnels["AA"]!!))
 
-    println(memo[2])
-
+//    println(memo[2])
+    var count = 0
+    for (m in memo) {
+        for (t in m) {
+            for (t2 in t) {
+                for (h in t2.value) {
+                    count++
+                }
+            }
+        }
+    }
+    println(count)
 }
 
-data class Tunnel(val name: String, val neighbors: HashSet<String>, val flowRate: Int, var dist: HashMap<String, Int>)
+data class Tunnel(
+    val name: String,
+    val neighbors: HashSet<String>,
+    val flowRate: Int,
+    var dist: SortedMap<Int, HashSet<String>>
+)
 
 fun calcDist(tunnels: HashMap<String, Tunnel>, start: Tunnel, tunnel: Tunnel, seen: HashSet<String>, sum: Int) {
-    if (seen.contains(tunnel.name)) {
-        return
+    if (start.name == "BB") {
+        println()
     }
-    seen.add(tunnel.name)
+    var q = LinkedList<Tunnel>()
+    var q2 = LinkedList<Tunnel>()
 
-    for(t in tunnel.neighbors) {
-        println(t)
+    q.addLast(start)
+    var count = 0
 
-        start.dist[t] = Math.min(start.dist[t] ?: Int.MAX_VALUE, sum + 1)
-        calcDist(tunnels, start, tunnels.get(t)!!, seen, sum + 1)
+    while (q.isNotEmpty()) {
+        val top = q.pop()
+        if (seen.contains(top.name)) {
+            continue
+        }
+        seen.add(top.name)
+        if (top.flowRate != 0 && top.name != start.name) {
+            start.dist.putIfAbsent(count, HashSet())
+            start.dist[count]!!.add(top.name)
+        }
 
+        for (t in top.neighbors) {
+            if (!seen.contains(t)) {
+                q2.addLast(tunnels[t]!!)
+            }
+        }
+
+        if (q.isEmpty() && q2.isNotEmpty()) {
+            count++
+            val temp = q
+            q = q2
+            q2 = temp
+        }
     }
+
+//    for(t in tunnel.neighbors) {
+//        println(t)
+//
+//        start.dist[t] = Math.min(start.dist[t] ?: Int.MAX_VALUE, sum + 1)
+//        calcDist(tunnels, start, tunnels.get(t)!!, seen, sum + 1)
+//    }
 }
+
 var count = 0
 var memoCount = 0
+val maxTime = 26
 
-fun calcMax(time: Int, tunnel: Tunnel, tunnels: HashMap<String, Tunnel>, memo: ArrayList<HashMap<HashSet<String>, HashMap<HashSet<String>, Int>>>, on: HashSet<String>, eTunnel: Tunnel): Int {
-    if((time == 26) || on.size == 15) {
+fun calcMax(
+    time: Int,
+    eTime: Int,
+    tunnel: Tunnel,
+    tunnels: HashMap<String, Tunnel>,
+    memo: ArrayList<ArrayList<HashMap<HashSet<String>, HashMap<Pair<String, String>, Int>>>>,
+    on: HashSet<String>,
+    eTunnel: Tunnel
+): Int {
+    if ((time >= maxTime && eTime >= maxTime)) {
         return 0
     }
-
-
 //    println(memo[24].size)
 
 //    if(memo[time][on]?.containsKey(Pair(tunnel.name, eTunnel.name)) == true) {
 //        return memo[time][on]?.get(Pair(tunnel.name, eTunnel.name))!!
 //    }
 
-
-
 //    val memoCheck = on.reduce { i, j -> i + j }
 
 //    println(on.size)
 
-    if(memo[time][on]?.containsKey(setOf(tunnel.name, eTunnel.name)) == true) {
+//    if(time == maxTime && eTime == 25 && on == hashSetOf("", "DD") && tunnel == tunnels["DD"] && eTunnel == tunnels["BB"]) {
+//        println("here")
+//    }
+//    println(eTunnel.name)
+    if (memo[time][eTime][on]?.containsKey(Pair(tunnel.name, eTunnel.name)) == true) {
+//        println("memo hit")
 //        println("memo ${++memoCount}")
-        return memo[time][on]?.get(setOf(tunnel.name, eTunnel.name))!!
-    }
-
-    var canMove = false
-    for(a in tunnel.dist) {
-        if(a.value < 26 - time - 1 && !on.contains(a.key)) {
-            canMove = true
+        ++memoCount
+        if (memoCount % 10000000 == 0) {
+            println("T: $time ET: $eTime on: $on ${setOf(tunnel.name, eTunnel.name)}")
+            println(memoCount)
         }
+        return memo[time][eTime][on]?.get(Pair(tunnel.name, eTunnel.name))!!
     }
+//    else {
+//        val max = memo[time][eTime].filter { it.key.containsAll(on) }.mapNotNull {it.value.get(Pair(tunnel.name, eTunnel.name))}
+//        if(max.isNotEmpty()) {
+//            return max.max()
+//        }
+//    }
 
-    for(a in eTunnel.dist) {
-        if(a.value < 26 - time - 1 && !on.contains(a.key)) {
-            canMove = true
-        }
-    }
+//    var canMove = false
+//    for(a in tunnel.dist) {
+//        if(a.value < maxTime - time - 1 && !on.contains(a.key)) {
+//            canMove = true
+//        }
+//    }
 //
-    if(canMove == false) {
-        val onMoveClone = on.map { it }.toHashSet()
-        memo[time].putIfAbsent(onMoveClone, hashMapOf())
-        memo[time][onMoveClone]?.set(hashSetOf(tunnel.name, eTunnel.name), 0)
-        return 0
-    }
+//    for(a in eTunnel.dist) {
+//        if(a.value < maxTime - time - 1 && !on.contains(a.key)) {
+//            canMove = true
+//        }
+//    }
+////
+//    if(canMove == false) {
+//        val onMoveClone = on.map { it }.toHashSet()
+//        memo[time].putIfAbsent(onMoveClone, hashMapOf())
+//        memo[time][onMoveClone]?.set(hashSetOf(tunnel.name, eTunnel.name), 0)
+//        return 0
+//    }
 
-    count++
 
-    if(count % 100000 == 0) {
-        println(count)
-    }
 //    println(memo[time].size)
 
+//    if(time == 9 && tunnel.name == "JJ" && on.containsAll(listOf("DD", "BB"))) {
+//        println()
+//    }
+
+    var anyMove = false
     val selfOnly =
-        if (!on.contains(tunnel.name) && tunnel.flowRate != 0) {
+        if (tunnel.flowRate != 0 && time < maxTime && !on.contains(tunnel.name)) {
             var eMax = 0
             on.add(tunnel.name)
-            for (t in eTunnel.neighbors) {
-                val max = calcMax(time + 1, tunnel, tunnels, memo, on, tunnels[t]!!)
-                val selfOnLocal = tunnel.flowRate * (26 - time) + max
+            val toAdd = tunnel.flowRate * (maxTime - time)
+            var didMove = false
+            for (t in eTunnel.dist.headMap(maxTime - eTime - 1)) {
+                for (n in t.value) {
+                    if (!on.contains(n)) {
+                        didMove = true
+                        val max = calcMax(time + 1, eTime + t.key, tunnel, tunnels, memo, on, tunnels[n]!!)
+                        val selfOnLocal = toAdd + max
+                        if (selfOnLocal > eMax) {
+                            eMax = selfOnLocal
+                        }
+                    }
+                }
+            }
+
+            if (!didMove) {
+                val max = calcMax(time + 1, eTime, tunnel, tunnels, memo, on, eTunnel)
+                val selfOnLocal = toAdd + max
                 if (selfOnLocal > eMax) {
                     eMax = selfOnLocal
                 }
+                didMove = true
             }
             on.remove(tunnel.name)
+            anyMove = anyMove || didMove
             eMax
         } else {
             0
@@ -144,17 +230,35 @@ fun calcMax(time: Int, tunnel: Tunnel, tunnels: HashMap<String, Tunnel>, memo: A
 
 
     val elephantOnly =
-        if (!on.contains(eTunnel.name) && eTunnel.flowRate != 0) {
+        if (eTunnel.flowRate != 0 && eTime < maxTime && !on.contains(eTunnel.name)) {
             var eMax = 0
             on.add(eTunnel.name)
-            for (t in tunnel.neighbors) {
-                val max = calcMax(time + 1, tunnels[t]!!, tunnels, memo, on, eTunnel)
-                val selfOnLocal = eTunnel.flowRate * (26 - time) + max
+            val toAdd = eTunnel.flowRate * (maxTime - eTime)
+            var didMove = false
+
+            for (t in tunnel.dist.headMap(maxTime - time - 1)) {
+                for (n in t.value) {
+                    if (!on.contains(n)) {
+                        didMove = true
+                        val max = calcMax(time + t.key, eTime + 1, tunnels[n]!!, tunnels, memo, on, eTunnel)
+                        val selfOnLocal = toAdd + max
+                        if (selfOnLocal > eMax) {
+                            eMax = selfOnLocal
+                        }
+                    }
+                }
+            }
+
+            if (!didMove) {
+                val max = calcMax(time, eTime + 1, tunnel, tunnels, memo, on, eTunnel)
+                val selfOnLocal = toAdd + max
                 if (selfOnLocal > eMax) {
                     eMax = selfOnLocal
                 }
+                didMove = true
             }
             on.remove(eTunnel.name)
+            anyMove = anyMove || didMove
             eMax
         } else {
             0
@@ -162,30 +266,69 @@ fun calcMax(time: Int, tunnel: Tunnel, tunnels: HashMap<String, Tunnel>, memo: A
 
 
     val both =
-        if (!on.contains(tunnel.name) && tunnel.flowRate != 0
-            && !on.contains(eTunnel.name) && eTunnel.flowRate != 0 && tunnel != eTunnel
-        ) {
+        if (tunnel != eTunnel && eTime < maxTime && time < maxTime && !on.contains(eTunnel.name) && !on.contains(tunnel.name)) {
             on.add(eTunnel.name)
             on.add(tunnel.name)
-            val max = calcMax(time + 1, tunnel, tunnels, memo, on, eTunnel)
-            val selfOnLocal = eTunnel.flowRate * (26 - time) + tunnel.flowRate * (26 - time) + max
+            val max = calcMax(time + 1, eTime + 1, tunnel, tunnels, memo, on, eTunnel)
+            val selfOnLocal = eTunnel.flowRate * (maxTime - eTime) + tunnel.flowRate * (maxTime - time) + max
             on.remove(tunnel.name)
             on.remove(eTunnel.name)
+            anyMove = true
             selfOnLocal
         } else {
             0
         }
 
     var maxTravel = 0
-    for(t in tunnel.neighbors) {
-        for(et in eTunnel.neighbors) {
-            val max = calcMax(time + 1, tunnels[t]!!, tunnels, memo, on, tunnels[et]!!)
-            if (max > maxTravel) {
-                maxTravel = max
+    var didMove = false
+    if (!anyMove) {
+        for (t in tunnel.dist.headMap(maxTime - time - 1)) {
+            for (n in t.value) {
+                if (!on.contains(n)) {
+                    for (et in eTunnel.dist.headMap(maxTime - eTime - 1)) {
+                        for (n2 in et.value) {
+                            if (!on.contains(n2)) {
+                                didMove = true
+                                val max = calcMax(
+                                    time + t.key,
+                                    eTime + et.key,
+                                    tunnels[n]!!,
+                                    tunnels,
+                                    memo,
+                                    on,
+                                    tunnels[n2]!!
+                                )
+                                if (max > maxTravel) {
+                                    maxTravel = max
+                                }
+                            }
+                        }
+                        if (!didMove) {
+                            val max = calcMax(time + t.key, eTime, tunnels[n]!!, tunnels, memo, on, eTunnel)
+                            if (max > maxTravel) {
+                                maxTravel = max
+                            }
+                            didMove = true
+                        }
+                    }
+                }
             }
         }
-    }
 
+        if (!didMove) {
+            for (et in eTunnel.dist.headMap(maxTime - eTime - 1)) {
+                for (n in et.value) {
+                    if (!on.contains(n)) {
+                        val max = calcMax(time, eTime + et.key, tunnel, tunnels, memo, on, tunnels[n]!!)
+                        if (max > maxTravel) {
+                            maxTravel = max
+                        }
+                    }
+                }
+            }
+        }
+
+    }
     val max = maxOf(maxTravel, selfOnly, elephantOnly, both)
 
 //    var string = ""
@@ -196,11 +339,22 @@ fun calcMax(time: Int, tunnel: Tunnel, tunnels: HashMap<String, Tunnel>, memo: A
 //           hashSetOf(tunnel.name, eTunnel.name),
 //            maxOf(max, memo[time][string]?.get(hashSetOf(tunnel.name, eTunnel.name)) ?: 0 ))
 //    }
-    val onClone = on.map { it }.toHashSet()
 
-    memo[time].putIfAbsent(onClone, hashMapOf())
-    memo[time][onClone]?.set(hashSetOf(tunnel.name, eTunnel.name), max)
+    count++
+//    println("$time $eTime")
+    if (count % 1000000 == 0) {
+        println("$time $eTime")
+        println(count)
+    }
+    val onClone = on.map { it }.toHashSet()
+//    if(time == maxTime && eTime == 25 && on == hashSetOf("", "DD") && tunnel == tunnels["DD"] && eTunnel == tunnels["BB"]) {
+//        println("here")
+//    }
+
+    memo[time][eTime].putIfAbsent(onClone, hashMapOf())
+    memo[time][eTime][onClone]?.set(Pair(tunnel.name, eTunnel.name), max)
 
     return max
 }
+
 
---
+++ src/main/kotlin/Day17.kt
@@ -0,0 +1,149 @@
+import java.io.File
+
+fun main() {
+    val lines = File("Day17.csv").useLines { it.toList() }
+    val dirs = ArrayList<RockDir>()
+    for(c in lines.first()) {
+        val newDir = when(c) {
+            '>' -> RockDir.RIGHT
+            '<' -> RockDir.LEFT
+            else -> RockDir.LEFT
+        }
+        dirs.add(newDir)
+    }
+
+    val grid = ArrayList<ArrayList<Boolean>>()
+    for(i in 0 until 3) {
+        grid.add(arrayListOf(false, false, false, false, false, false, false))
+    }
+    var top = -1
+    var counter = 0
+
+    val history = HashMap<Triple<ArrayList<Boolean>, RockShape, Int>, HashSet<Pair<Int, Int>>>()
+    val historyTop = ArrayList<Int>()
+
+    for(i in 0 until 100_000_000) {
+        val shape = ROCK_ORDER[i % 5]
+        val llY = top + 4
+        val rock = Rock(Pair(llY, 2), shape)
+        while(grid.size <= rock.top()) {
+            grid.add(arrayListOf(false, false, false, false, false, false, false))
+        }
+
+        var vertical = false
+        while(!vertical || rock.tryDown(grid)) {
+            if(vertical) {
+                // we can move down
+                rock.moveDown(grid)
+            } else {
+                val dir = dirs[counter % dirs.size]
+                rock.moveDir(grid, dir)
+                counter++
+            }
+            vertical = !vertical
+        }
+
+        rock.drawRock(grid)
+        top = maxOf(rock.top(), top)
+        historyTop.add(top)
+        if(history.contains(Triple(grid[top], shape, counter % dirs.size))) {
+            val hist = history[Triple(grid[top], shape, counter % dirs.size)]?.first()
+            if(hist != null) {
+                val intMult = 1000000000000/(i + 1 - hist.first)
+                val math = (top + 1 - hist.second)
+                val addI = (1000000000000 - intMult * (i + 1 - hist.first) - hist.first).toInt()
+                val valAt = historyTop[addI + hist.first-1] - historyTop[hist.first-1]
+                println(i)
+                println(intMult * math + hist.second + valAt)
+                break
+            }
+        }
+        history.putIfAbsent(Triple(grid[top], shape, counter % dirs.size), hashSetOf())
+        history[Triple(grid[top], shape, counter % dirs.size)]?.add(Pair(i + 1, top + 1))
+
+    }
+//    grid.draw()
+
+}
+
+fun ArrayList<ArrayList<Boolean>>.draw() {
+    for(i in size - 1 downTo 0) {
+        for(j in 0 until 7) {
+            if(this[i][j]) {
+                print("#")
+            } else {
+                print(".")
+            }
+        }
+        println()
+    }
+}
+
+val ROCK_ORDER = arrayListOf<RockShape>(RockShape.FLAT, RockShape.PLUS, RockShape.EL, RockShape.VERTICAL, RockShape.SQUARE)
+
+data class Rock(var ll: Pair<Int, Int>, val shape: RockShape) {
+    fun allCoords(pos: Pair<Int, Int> = ll): ArrayList<Pair<Int, Int>> {
+        val coords = ArrayList<Pair<Int, Int>>()
+        for(p in shape.points) {
+            val newCoord = Pair(pos.first + p.first, pos.second + p.second)
+            coords.add(newCoord)
+        }
+        return coords
+    }
+
+    fun tryDown(grid: ArrayList<ArrayList<Boolean>>): Boolean {
+        val coords = allCoords(Pair(ll.first - 1, ll.second))
+        if(coords.any {it.first < 0 || grid[it.first][it.second]}) {
+            return false
+        }
+
+        return true
+    }
+
+    fun moveDown(grid: ArrayList<ArrayList<Boolean>>) {
+        if(tryDown(grid)) {
+            ll = Pair(ll.first - 1, ll.second)
+        }
+    }
+
+    fun tryDir(grid: ArrayList<ArrayList<Boolean>>, dir: RockDir): Boolean {
+        val coords = allCoords(Pair(ll.first, ll.second + dir.dir))
+        if(coords.any {it.second > 6 || it.second < 0 || grid[it.first][it.second]}) {
+            return false
+        }
+
+        return true
+    }
+
+    fun moveDir(grid: ArrayList<ArrayList<Boolean>>, dir: RockDir) {
+        if(tryDir(grid, dir)) {
+            ll = Pair(ll.first, ll.second + dir.dir)
+        }
+    }
+
+    fun drawRock(grid: ArrayList<ArrayList<Boolean>>) {
+        while(top() >= grid.size) {
+            grid.add(arrayListOf(false, false, false, false, false, false, false))
+        }
+
+        val coords = allCoords()
+        for(c in coords) {
+            grid[c.first][c.second] = true
+        }
+    }
+
+    fun top() = allCoords().maxOf { it.first }
+}
+
+enum class RockShape(val points: ArrayList<Pair<Int, Int>>) {
+    FLAT(arrayListOf(0 to 0, 0 to 1, 0 to 2, 0 to 3)),
+    PLUS(arrayListOf(1 to 0, 0 to 1, 1 to 1, 2 to 1, 1 to 2)),
+    EL(arrayListOf(0 to 0, 0 to 1, 0 to 2, 1 to 2, 2 to 2)),
+    VERTICAL(arrayListOf(0 to 0, 1 to 0, 2 to 0, 3 to 0)),
+    SQUARE(arrayListOf(0 to 0, 1 to 0, 0 to 1, 1 to 1))
+}
+
+enum class RockDir(val dir: Int) {
+    LEFT(-1),
+    RIGHT(1)
+}
